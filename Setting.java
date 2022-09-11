536038a023da23d16a83f0c9cce546b6142b5098
cari-ri <112920752+cari-ri@users.noreply.github.com>
2022-09-11 17:43:41 -0600

    Update Setting.java

--- Setting.java
+++ Setting.java
@@ -1,14 +1,30 @@
+import java.util.*;
 
-public enum Setting {OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
-	private String whoKnows;
-	
-	Setting (String quien) {
-		whoKnows = quien;
+public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+
+	private String whoKnows; // instance variable
+	private HashMap<String, String> temps = new HashMap<String, String>();
+	private String addMe = "";
+
+	Setting(String quien) {
+		whoKnows = quien; // constructor that accepts a string
+
+		temps.put("OFF", "coool");
+		temps.put("LOW", "warm");
+		temps.put("MEDIUM", "CAREFUL");
+		temps.put("HIGH", "VERY HOT! DON'T TOUCH");
+
 	}
 
 	@Override
-	public String toString() {
-		return "Setting [whoKnows=" + whoKnows + "]";
+	public String toString() { // toString() function
+		// switch(Setting) {
+		// case Setting == Setting.OFF:
+
+		// }
+		return "[" + this.whoKnows + "]....." + temps.get(this.whoKnows);
 	}
-	
+
 }
+
