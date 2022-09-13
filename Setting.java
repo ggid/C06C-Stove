44c4e97f7cf59a600d402958d63676f0d8dbb097
cari-ri <112920752+cari-ri@users.noreply.github.com>
2022-09-13 10:56:03 -0600

    FINAL Setting

--- Setting.java
+++ Setting.java
@@ -1,11 +1,8 @@
-import java.util.*;
 
 public enum Setting {
 	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
 
 	private String whoKnows; // instance variable
-	private HashMap<String, String> temps = new HashMap<String, String>();
-	private String addMe = "";
 
 	Setting(String quien) {
 		this.whoKnows = quien; // constructor that accepts a string
@@ -14,23 +11,7 @@ public enum Setting {
 
 	@Override
 	public String toString() { // toString() function
-
-		this.temps.put("OFF", "coool");
-		this.temps.put("LOW", "warm");
-		this.temps.put("MEDIUM", "CAREFUL");
-		this.temps.put("HIGH", "VERY HOT! DON'T TOUCH");
-
-		switch (this.whoKnows) {
-		case "---":
-			return "[" + this.whoKnows + "]....." + temps.get("OFF");
-		case "--+":
-			return "[" + this.whoKnows + "]....." + temps.get("LOW");
-		case "-++":
-			return "[" + this.whoKnows + "]....." + temps.get("MEDIUM");
-		case "+++":
-			return "[" + this.whoKnows + "]....." + temps.get("HIGH");
-		}
-		return ":(";
+		return this.whoKnows;
 	}
 
 }
